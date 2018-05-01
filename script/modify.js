135cda9ae4f141be114a38b3abb72289a2e7d423
tigercosmos <phy.tiger@gmail.com>
2018-05-01 11:38:07 +0800

    modify all html

--- script/modify.js
+++ script/modify.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 
-const filePath = path.join(__dirname, '../_book/index.html');
-let data = fs.readFileSync(filePath, 'utf-8');
-
-data = data.replace("Published with GitBook", '');
-tmp = data.split("</head>");
-data = tmp[0] + `
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-84804118-4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-84804118-4');
-</script>
-</head>` + tmp[1];
-
-fs.writeFileSync(filePath, data, 'utf-8');
+const dirPath = path.join(__dirname, '../_book/');
+const dirCont = fs.readdirSync(dirPath);
+const files = dirCont.filter((elm) => elm.match(/\.html$/));
+files.forEach(file => {
+    const filePath = path.join(dirPath, file);
+    let data = fs.readFileSync(filePath, 'utf-8');
+    data = data.replace("Published with GitBook", '');
+    tmp = data.split("</head>");
+    data = tmp[0] + `<script async src="https://www.googletagmanager.com/gtag/js?id=UA-84804118-4"></script><script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'UA-84804118-4');</script></head>` +
+        tmp[1];
+    fs.writeFileSync(filePath, data, 'utf-8');
+    console.log(file, "has been modified.");
+});
